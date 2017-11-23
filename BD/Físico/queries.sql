ebb38e176182c8bf78936919bc108720ee18a2af
Diana Costa <dianasofia.costa@sapo.pt>
2017-11-23 13:39:43 +0000

    [BD] queries doneeeeeee

--- BD/Físico/queries.sql
+++ BD/Físico/queries.sql
@@ -2,13 +2,12 @@
  Os alunos devem ser capazes de obter a lista de cadeiras 
  a que estão inscritos.
 */
-
 DROP PROCEDURE IF EXISTS lista_cadeiras;
 DELIMITER $$
 CREATE PROCEDURE lista_cadeiras(id_aluno INT)
 BEGIN
 	SELECT UC.Nome FROM UC
-		JOIN UCAluno ON UCAluno.UC_Codigo = UC.Codigo
+		INNER JOIN UCAluno ON UCAluno.UC_Codigo = UC.Codigo
 		WHERE UCAluno.Aluno_Numero = id_aluno;
 END
 $$
@@ -28,18 +27,99 @@ DELIMITER $$
 CREATE PROCEDURE lista_turnos(id_aluno INT)
 BEGIN
 	SELECT UC.Nome, UCAluno.Turno FROM UCAluno
-		JOIN UC ON UC.Codigo = UCAluno.UC_Codigo 
+		INNER JOIN UC ON UC.Codigo = UCAluno.UC_Codigo 
 		WHERE UCAluno.Aluno_Numero = id_aluno;
 END
 $$
 DELIMITER ;
 
 CALL lista_turnos(78985);
+
 -- --------------------------------------------------------------------
+/*
+ Os docentes devem ser capazes de obter a lista dos alunos das UC's
+ que lecionam.
+*/
+DROP PROCEDURE IF EXISTS lista_alunos;
+DELIMITER $$
+CREATE PROCEDURE lista_alunos(id_docente INT)
+BEGIN
+	SELECT Aluno.Nome, UC.Nome FROM Aluno
+    INNER JOIN UCAluno ON UCAluno.Aluno_Numero = Aluno.Numero
+    INNER JOIN UC ON UC.Codigo = UCAluno.UC_Codigo
+    INNER JOIN DocenteUC ON DocenteUC.UC_Codigo = UC.Codigo
+    WHERE DocenteUC.Docente_Numero = id_docente;
+END
+$$
+DELIMITER ;
 
--- funcao procedure de ver qual cadeira q tem mais alunos inscritos
+CALL lista_alunos(79194);
 
--- codigo de calculo: 423
-SELECT Docente.Nome FROM Docente
-	INNER JOIN DocenteUC ON Docente.Numero = DocenteUC.Docente_Numero
-	WHERE DocenteUC.UC_Codigo = 423;
+-- --------------------------------------------------------------------
+/*
+ Os docentes devem ser capazes de obter a lista dos alunos das UC's
+ que lecionam.
+*/
+DROP PROCEDURE IF EXISTS lista_alunos;
+DELIMITER $$
+CREATE PROCEDURE lista_alunos(id_docente INT)
+BEGIN
+	SELECT Aluno.Nome, UC.Nome FROM Aluno
+    INNER JOIN UCAluno ON UCAluno.Aluno_Numero = Aluno.Numero
+    INNER JOIN UC ON UC.Codigo = UCAluno.UC_Codigo
+    INNER JOIN DocenteUC ON DocenteUC.UC_Codigo = UC.Codigo
+    WHERE DocenteUC.Docente_Numero = id_docente;
+END
+$$
+DELIMITER ;
+
+CALL lista_alunos(79194);
+
+
+-- ----------------------------------------------------------------------
+/*
+ O diretor de curso deve ser capaz de obter a listagem de todos os 
+ alunos do curso.
+*/
+SELECT Aluno.Numero, Aluno.Nome FROM Aluno;
+
+
+-- ----------------------------------------------------------------------
+/*
+ O diretor de curso deve ser capaz de aceder à lista
+ de alunos e respectivas cadeiras.
+*/
+SELECT Aluno.Numero, Aluno.Nome, UC.Nome FROM Aluno
+	INNER JOIN UCAluno ON UCAluno.Aluno_Numero = Aluno.Numero
+    INNER JOIN UC ON UC.Codigo = UCAluno.UC_Codigo;
+
+
+-- ----------------------------------------------------------------------    
+ /*
+ O diretor de curso deve conseguir saber o total de UC's que
+ estão a ser lecionadas no curso.
+*/   
+SELECT COUNT(Codigo) AS numero_ucs FROM UC;
+
+-- ----------------------------------------------------------------------    
+ /*
+ O diretor de curso deve conseguir saber o total de Alunos
+ que o curso tem atualmente
+*/   
+SELECT COUNT(Numero) AS numero_alunos FROM Aluno;
+
+-- ----------------------------------------------------------------------    
+ /*
+ O diretor de curso deve ser capaz de saber qual o top 3
+ de cadeiras com mais alunos inscritos, e respetivos docentes.
+*/   
+SELECT UC.Nome, COUNT(*), Docente.Nome FROM UC
+	INNER JOIN UCAluno ON UCAluno.UC_Codigo = UC.Codigo
+    INNER JOIN Aluno ON Aluno.Numero = UCAluno.Aluno_Numero
+    INNER JOIN DocenteUC ON DocenteUC.UC_Codigo = UC.Codigo
+    INNER JOIN Docente ON Docente.Numero = DocenteUC.Docente_Numero
+    GROUP BY UC.Nome
+    ORDER BY COUNT(*) DESC
+    LIMIT 3;
+
+-- -----------------------------------------------------------------------
