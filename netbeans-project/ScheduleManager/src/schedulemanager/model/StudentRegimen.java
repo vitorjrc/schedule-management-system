49db34087e584038de3665f4262790a7feca9486
marcospgp <marcospereiradev@gmail.com>
2017-11-25 20:21:52 +0000

    Student work

--- netbeans-project/ScheduleManager/src/schedulemanager/model/StudentRegimen.java
+++ netbeans-project/ScheduleManager/src/schedulemanager/model/StudentRegimen.java
@@ -1,23 +1,15 @@
 package schedulemanager.model;
 
 public enum StudentRegimen {
-    STUDENT, WORKERSTUDENT;
+
+	STUDENT, WORKERSTUDENT;
     
-    public static StudentRegimen convert(String str) {
-        for (StudentRegimen regimen : StudentRegimen.values()) {
-            if (regimen.toString().equals(str)) {
-                return regimen;
-            }
-        }
-        return null;
+    @Override
+    public String toString() {
+	    switch(this) {
+	      case STUDENT: return "Regime Normal";
+	      case WORKERSTUDENT: return "Trabalhador-Estudante";
+	      default: throw new IllegalArgumentException();
+	    }
     }
-    
- @Override
-  public String toString() {
-    switch(this) {
-      case STUDENT: return "Regime Normal";
-      case WORKERSTUDENT: return "Trabalhador-Estudante";
-      default: throw new IllegalArgumentException();
-    }
-  }
 }
