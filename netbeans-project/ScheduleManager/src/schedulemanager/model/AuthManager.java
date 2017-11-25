bdcbe752da10deaa3d74ce7c3812f27feacd85a2
marcospgp <marcospereiradev@gmail.com>
2017-11-25 21:00:02 +0000

    Finish up AuthManager

--- netbeans-project/ScheduleManager/src/schedulemanager/model/AuthManager.java
+++ netbeans-project/ScheduleManager/src/schedulemanager/model/AuthManager.java
@@ -12,6 +12,8 @@ public class AuthManager {
 	private Student loggedInStudent = null;
 	private ArrayList<Student> registeredStudents;
 	
+	// Admin login credentials. Not safe in current form.
+	private static final String adminID = "admin";
 	private static final String adminPassword = "SECURITYHOLE";
 	
 	public AuthManager() {
@@ -25,20 +27,72 @@ public class AuthManager {
 		registeredStudents.add(newStudent);
 	}
 	
-	public void login() {
-		// todo
+	// Returns true if login was successful, false otherwise
+	public boolean login(String id, String password) {
+		
+		// Check if this is an admin login attempt
+		if (id == this.adminID) {
+			if (password == this.adminPassword) {
+				
+				this.isAdminLoggedIn = true;
+				this.isStudentLoggedIn = false;
+				this.loggedInStudent = null;
+				
+				return true;
+			
+			} else {
+				
+				return false; // Tried to login as admin but password is wrong
+			}
+		}
+		
+		// Check if a student exists with the given ID
+		for (Student student: this.registeredStudents) {
+
+			if (student.getID() == id) {
+				
+				// Check password
+				if (student.getPassword() == password) {
+					
+					this.isStudentLoggedIn = true;
+					this.isAdminLoggedIn = false; // Just to make sure
+					
+					this.loggedInStudent = student;
+					
+					return true;
+					
+				} else {
+					
+					// Here we can add extra wrong password logic,
+					// such as counting failed attempts
+					return false;
+				}
+			}
+		}
+		
+		return false; // Didn't find student with received ID
+	}
+	
+	public void logout() {
+		
+		this.isAdminLoggedIn = false;
+		this.isStudentLoggedIn = false;
+		this.loggedInStudent = null;
 	}
 	
 	public boolean isStudentLoggedIn() {
-		// todo
+		
+		return this.isStudentLoggedIn;
 	}
 	
 	public boolean isAdminLoggedIn() {
-		// todo
+		
+		return this.isAdminLoggedIn;
 	}
 	
 	public Student getLoggedInStudent() {
-		// todo
+		
+		return this.loggedInStudent.clone();
 	}
 
 }
