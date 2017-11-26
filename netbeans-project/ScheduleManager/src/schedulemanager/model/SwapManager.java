f6ba9098a2cf63da0ea9153bcd5db563df018814
marcospgp <marcospereiradev@gmail.com>
2017-11-26 01:00:33 +0000

    SwapManager work

--- netbeans-project/ScheduleManager/src/schedulemanager/model/SwapManager.java
+++ netbeans-project/ScheduleManager/src/schedulemanager/model/SwapManager.java
@@ -17,32 +17,61 @@ public class SwapManager {
 	private HashMap<String, HashMap<String, Swap>> swapsByStudentID; // studentID -> (swapID -> Swap)
 	
 	private boolean swapsAllowed = true; // Keeps track of whether Swaps are still allowed.
+	private AuthManager authManager;     // Keeps an instance of authManager to check student info
 	
-	public SwapManager() {
+	public SwapManager(AuthManager authManager) {
+		this.authManager = authManager;
 		this.swapsByStudentID = new HashMap<String, HashMap<String, Swap>>();
 	}
 	
-	public SwapManager(boolean swapsAllowed) {
+	public SwapManager(AuthManager authManager, boolean swapsAllowed) {
+		this.authManager = authManager;
 		this.swapsByStudentID = new HashMap<String, HashMap<String, Swap>>();
 		this.swapsAllowed = swapsAllowed;
 	}
+	
+	// Checks wether a swap offer is legal, given a bidder and the courseID & shiftID of the offered shift
+	private boolean swapOfferAllowed(String bidderID, String courseID, String offeredShiftID) {
+		
+		// Check that bidder is the student that is logged in
+		if (this.authManager.getLoggedInStudent().getID() != bidderID) {
+			
+			return false;
+		}
+		
+		// Check that bidder has this shift to offer
+		if (!this.authManager.getLoggedInStudent().hasShift(courseID, offeredShiftID)) {
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+	// Creates a given student's HashMap of swaps if it doesn't exist
+	private void createStudentSwapsMapIfNotExists(String studentID) {
+		
+        if (!this.swapsByStudentID.containsKey(studentID)) {
+			
+			this.swapsByStudentID.put(studentID, new HashMap<String, Swap>());
+		}
+	}
 
-	// Creates a swap offer without checking if the student has the shift
-	// they are offering. This check should be done beforehand.
+	// Creates a swap offer
 	// Returns true if swap offer is created successfully, false otherwise
 	public boolean createSwapOffer(String bidderID, String courseID, String offeredShiftID, String wantedShiftID) {
 		
-		// Create student swaps HashMap if it doesn't exist yet
-		if (!this.swapsByStudentID.containsKey(bidderID)) {
-			
-			this.swapsByStudentID.put(bidderID, new HashMap<String, Swap>());
+		if (!this.swapsAllowed || !this.swapOfferAllowed(bidderID, courseID, offeredShiftID)) {
+			return false;
 		}
 		
-		// Create new Swap
-		Swap newSwap = new Swap(bidderID, courseID, offeredShiftID, wantedShiftID);
+		// Create this student's swaps HashMap if it doesn't exist yet
+		this.createStudentSwapsMapIfNotExists(bidderID);
 		
-		// Get bidderSwaps HashMap
+		// Get bidder's swaps
 		HashMap<String, Swap> bidderSwaps = this.swapsByStudentID.get(bidderID);
+		
+		Swap newSwap = new Swap(bidderID, courseID, offeredShiftID, wantedShiftID);
 		
 		// Check that this offer doesn't already exist
 		if (bidderSwaps.containsKey(newSwap.getID())) {
@@ -60,13 +89,13 @@ public class SwapManager {
 	// Cancels a swap offer. Returns true if successful.
 	public boolean cancelSwapOffer(String studentID, String swapID) {
 		
-		if (!this.swapsByStudentID.containsKey(studentID)) {
+		if (studentID != this.authManager.getLoggedInStudent().getID()) {
 			return false;
 		}
 		
 		HashMap<String, Swap> swaps = this.swapsByStudentID.get(studentID);
 		
-		if (!swaps.containsKey(swapID)) {
+		if (swaps == null || !swaps.containsKey(swapID)) {
 			return false;
 		}
 		
