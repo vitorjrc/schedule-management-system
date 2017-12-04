a3452b4d823ab98da3f0a67ef80df3f8cc2c34e8
Sérgio Oliveira <stoj97@gmail.com>
2017-12-04 11:56:04 +0000

    [DSS] - Connection to DB done

---
+++ netbeans-project/ScheduleManager/src/schedulemanager/model/StudentDAO.java
@@ -0,0 +1,15 @@
+package schedulemanager.model;
+
+import java.util.Map;
+
+public class StudentDAO implements Map<String, Student> {
+    
+    
+    @Override
+    public int size() {
+    
+    
+        
+    }
+    
+}
