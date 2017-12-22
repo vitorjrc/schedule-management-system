9d04d892cd338264c42794bd04ef484f2dfbd44a
Sérgio Oliveira <stoj97@gmail.com>
2017-12-22 13:25:14 +0000

    [DSS] - minor update

--- DSS/Database/bdCreation.sql
+++ DSS/Database/bdCreation.sql
@@ -43,7 +43,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `mydb`.`course` (
   `id` VARCHAR(45) NOT NULL,
   `name` VARCHAR(90) NOT NULL,
-  `teacher_id` VARCHAR(45) NOT NULL,
+  `teacher_id` VARCHAR(45) NULL,
   INDEX `fk_Courses_Teachers1_idx` (`teacher_id` ASC),
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_Courses_Teachers1`
