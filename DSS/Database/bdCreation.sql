63f3594f0dc35f9494877f81d61f15eb04d9db6b
Sérgio Oliveira <stoj97@gmail.com>
2017-12-26 21:19:26 +0000

    [DSS] - BD updated

--- DSS/Database/bdCreation.sql
+++ DSS/Database/bdCreation.sql
@@ -156,6 +156,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`student_course` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `mydb`.`config`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`config` (
+  `name` VARCHAR(45) NOT NULL,
+  `value` INT NOT NULL DEFAULT 1,
+  PRIMARY KEY (`name`))
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
